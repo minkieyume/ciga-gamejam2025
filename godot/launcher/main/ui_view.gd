21eaa78866901caada2372348f22bbb281f326c3
MinkieYume <minkieyume@gmail.com>
2025-06-28 15:35:16 +0800

    ui逻辑实现

--- godot/launcher/main/ui_view.gd
+++ godot/launcher/main/ui_view.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var main_ui = $MainUI
 @onready var fail_ui = $FailUi
+@onready var pause_ui = $PauseUI
 @onready var ballon = $Ballon
 
 func get_ballon():
@@ -10,3 +11,6 @@ func get_ballon():
 func _on_fail_ui_back_menu() -> void:
 	fail_ui.visible = false
 	main_ui.visible = true
+
+func pause() -> void:
+	pause_ui.visible = true
