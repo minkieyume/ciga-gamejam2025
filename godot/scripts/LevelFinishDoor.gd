9120a50131bb505978485b434fee9dbecdb7fd8e
MinkieYume <minkieyume@gmail.com>
2025-06-29 08:58:51 +0800

    组建关卡

--- godot/scripts/LevelFinishDoor.gd
+++ godot/scripts/LevelFinishDoor.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-# Define the next scene to load in the inspector
-@export var next_scene : PackedScene
+func _ready() -> void:
+	$Sprite2D.play(&"default")
 
 # Load next level scene when player collide with level finish door.
 func _on_body_entered(body):
